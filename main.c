988f79b86a4da3287540a1f8d21307b3e1097dc8
Gero Zayas <gerozayas@gmail.com>
2026-09-23 20:59:42 +0200

    update

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  printf("%s\n", "HELLOPE WORLD (gingerBill's style)");
-  return 0;
+    printf("%s\n", "HELLOPE WORLD (gingerBill's style)");
+    return 0;
 }
